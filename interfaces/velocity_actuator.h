4fca851564eb5944ba3f0d7b1bac36f4690132b6
fabbricca <fabb.riccardo@gmail.com>
2026-01-31 21:23:31 +0100

    fix: remove brake method

--- interfaces/velocity_actuator.h
+++ interfaces/velocity_actuator.h
@@ -6,5 +6,4 @@ class VelocityActuatorInterface {
     public:
         virtual ~VelocityActuatorInterface() = default;
         virtual void applyVelocity(const Vector3D acceleration, const double max_velocity) const = 0;
-        virtual void brake(const double decel_magnitude) const = 0;  // Decelerate to stop
 };
